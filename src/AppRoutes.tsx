7516135593834e10ea6194065384efb4f9d778b9
Sagnik Mandal <acriticalcynic@outlook.com>
2024-08-01 01:01:27 +0530

    fix

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -26,7 +26,7 @@ function AppRoutes() {
           <Route path='/$2y$10$JHStxLISdZy1jdWJGO.ajeNv3Le6MmNz/oiOY5IcnAWN2UT7Q74Re' element={<LoadingPage />} />
           <Route path='/$2y$10$kJ5FxDIFuOHZJ5IT./p8h.wgZc7i6yZ9VDuzc6uFctqig9d5G.Yde' element={<LoadingPage />} />
           <Route path='/$2y$10$KLioYumGhY2jKZsejeJDOOaSDEk83UXLFQUKRQW8ezGi0nDDahLqC' element={<LoadingPage />} />
-          <Route path='/$2y$10$.SDpoZbCR/Xpf09Ao9SXBeMDDeZySjrSFI0uzFOyrU5ZM8H/pCmT.' element={<LoadingPage />} />
+          <Route path='/$2y$10$.SDpoZbCR/Xpf09Ao9SXBeMDDeZySjrSFI0uzFOyrU5ZM8H/pCmT' element={<LoadingPage />} />
           <Route path='/$2y$10$x7BEPL8CiA0BUcu/Fy8bWeIWb9p/IPr68fH2b7rhEqMJIYgHkPSc6' element={<LoadingPage />} />
           <Route path='/$2y$10$WextKBTIprT2Ee7wWOkgduYeZX4EhBjA7TNiSiVPblvMKK3tjWZSC' element={<LoadingPage />} />
           <Route path='/$2y$10$Eq/zi3VqBditD8d/JUEqyeAvHPCguLeKh1o.3DUKiyQ3OdSwDwNze' element={<LoadingPage />} />
