f170e0e22b0eda6f7f4aa3bbcad46e12f14ae3af
Sagnik Mandal <acriticalcynic@outlook.com>
2024-09-28 00:51:38 +0530

    something

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { dark } from '@clerk/themes'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 import AppRoutes from './AppRoutes'
+import  { Redirect } from 'react-router-dom'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY as string
 
@@ -11,16 +12,11 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
 }
 
+// redirect to localhost:8000, in any case
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ClerkProvider
-      publishableKey={PUBLISHABLE_KEY}
-      appearance={{
-        baseTheme: dark,
-      }}
-    >
-      <AppRoutes />
-    </ClerkProvider>
+    <Redirect to='http://localhost:8000' />
   </React.StrictMode>
 )
